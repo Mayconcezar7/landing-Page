aebce8048ed38bb6604d645ca23ba789508d15af
Maycon Cezar <maycon.cezar.souza7@gmail.com>
2025-08-11 18:40:27 -0300

    .

--- scripts/menuOptions.js
+++ scripts/menuOptions.js
@@ -1,44 +1,74 @@
 const menuOptions =  document.querySelectorAll(".item")
 const backgroundBlack = document.querySelector("#menu-options")
+const produtos  = document.querySelector(".lista-de-item")
 const produtionsBtnFechar =  document.querySelector(".produtions-btn-fechar")
 
 
+
+
+
+
+
+
+const showItens = (paragrafo)=>{
+
+   
+    produtos.innerHTML =""
+
+
+
+    let h2 = document.createElement("h2")
+
+
+    h2.innerHTML = paragrafo
+
+
+    produtos.appendChild(h2)
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 menuOptions.forEach(btnMenu =>{
-
-    //fundo desfocado
-
-    btnMenu.addEventListener("click",() =>{
-
-        backgroundBlack.style.display = "flex"
+   
+    btnMenu.addEventListener("click",(e) =>{
+        backgroundBlack.style.display = "flex"  
         
-          
-    })
-
-
-    produtionsBtnFechar.addEventListener("click",() =>{
-
-        backgroundBlack.style.display = "none"
-        
-          
-    })
-
-
-    backgroundBlack.addEventListener("click",(e) =>{
-
-        const listItens = document.querySelector("#produtos")
-
         
 
-        if(e.target=== backgroundBlack){
+        let categoria = e.target.closest(".item")
+
+        if(categoria){
+            let paragrafo = categoria.getAttribute("data-item")
+
+
+            showItens(paragrafo);
             
-            
-            backgroundBlack.style.display= "none"
-
         }
-          
     })
+ 
+})
 
 
+produtionsBtnFechar.addEventListener("click",() =>{
+        backgroundBlack.style.display = "none"       
+})
 
 
+backgroundBlack.addEventListener("click",(e) =>{
+        if(e.target=== backgroundBlack){   
+            backgroundBlack.style.display= "none"
+        }
 })
