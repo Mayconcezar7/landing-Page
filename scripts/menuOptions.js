90bf6da84cd0d8355b275cf5a7219cea734be94c
Maycon Cezar <maycon.cezar.souza7@gmail.com>
2025-08-15 12:02:50 -0300

    .

--- scripts/menuOptions.js
+++ scripts/menuOptions.js
@@ -1,7 +1,22 @@
+const produtosInformacao = [
+    //Bebidas quentes 
+    {imagem:"../imgProdutos/cafe.jpeg" ,alt:"bebida cafe", nome:"Café" , descricao:"Clássico global, pode ser puro, expresso, coado, cappuccino ou latte." , tamanho:"250ml", preco: 7.90 ,tipo:"Bebidas Quentes"},
+    {imagem:"../imgProdutos/cha.jpeg" ,alt:"bebida Chá", nome:"Chá" , descricao:"CPopular em inúmeras culturas, com variedades como preto, verde, camomila e chai." , tamanho:"250ml", preco: 6.90 ,tipo:"Bebidas Quentes"},
+    {imagem:"../imgProdutos/Cappuccino.jpeg" ,alt:"bebida Cappuccino", nome:"Cappuccino" , descricao:"Combinação italiana de café expresso, leite vaporizado e espuma de leite." , tamanho:"250ml", preco: 17.90 , tipo:"Bebidas Quentes"},
+    {imagem:"../imgProdutos/Chocolate-quente.jpeg" ,alt:"bebida Chocolate Quente", nome:"Chocolate Quente" , descricao:"Feito com leite e cacau, muito apreciado no inverno." , tamanho:"300ml", preco: 10.90 , tipo:"Bebidas Quentes"},
+    
+    //Bebidas Geladas
+
+    {imagem:"../imgProdutos/suco-de-laranja.jpg" ,alt:"Suco de laranja gelado", nome:"Suco de laranja gelado" , descricao:"Suco natural de laranja, refrescante e nutritivo." , tamanho:"500ml", preco: 23.90 ,tipo:"Bebidas Geladas"},
+]
+
 const menuOptions =  document.querySelectorAll(".item")
 const backgroundBlack = document.querySelector("#menu-options")
-const produtos  = document.querySelector(".lista-de-item")
+const produtos  = document.querySelector(".container-produtos")
 const produtionsBtnFechar =  document.querySelector(".produtions-btn-fechar")
+
+
+const tipoDeBebida = document.querySelector(".tipo-bebidas")
 
 
 
@@ -11,16 +26,43 @@ const produtionsBtnFechar =  document.querySelector(".produtions-btn-fechar")
 
 
 const showItens = (paragrafo)=>{
+    produtos.innerHTML = ""
 
    
+    const filtros = produtosInformacao.filter(itens => itens.tipo == paragrafo)
+
+    filtros.forEach((produtosItem) =>{
+
+        let divItens = document.createElement("div")
+        divItens.classList.add("produtos-item")
+
+
+        divItens.innerHTML = `
+                            <img src="${produtosItem.imagem}" alt="">
+                            <div class="container-descricao-item">
+                                <p class="descricao-item"> <strong>${produtosItem.nome}</strong> <br> <br> ${produtosItem.descricao} <br><strong>${produtosItem.tamanho}</strong></p>
+                                <p class="preco-item"> <strong>${produtosItem.preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong></p>
+                            </div>
+                            <button class="btn-add-carrinho" data-nome="${produtosItem.nome}" data-tamanho="${produtosItem.tamanho}" data-preco="${produtosItem.preco}" > <i class="fa fa-cart-plus"></i></button>
+
+        
+        `
+
+        produtos.appendChild(divItens)
+
+    })
+
+
     
-
-
-    produtos.appendChild(h2)
-
     
 
 }
+
+
+                    
+
+
+
 
 
 
@@ -39,7 +81,12 @@ menuOptions.forEach(btnMenu =>{
             let paragrafo = categoria.getAttribute("data-item")
 
 
+            
             showItens(paragrafo);
+
+            
+
+            
             
         }
     })
